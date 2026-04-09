1062d501112bb198214b8d21f841b4ccbfb08564
Anton <aa9953391@gmail.com>
2026-04-09 19:49:02 +0200

    Add Program.cs / Execution folder

---
+++ Execution/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using MedTenant.Application.Entities;
+using MedTenant.Infrastructure.Repositories;
+
+name space MedTenant
+{
+class Program
+{
+    static void Main(string[] args)
+    {
+        Doctor myDoctor = new Doctor("Anton", "Sheverdin", 1); // creating a doctor 
+        DoctorRepository repository = new DoctorRepository();
+
+        try
+        {
+            repository.AddDoctor(myDoctor);
+            Console.WriteLine("Doctor saved into database!");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Faiiled: " + ex.Message);
+        }
+    }
+}
+}
