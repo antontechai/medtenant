501f4eede75e4f2f9097dd099d199b2b67cd124e
Anton <aa9953391@gmail.com>
2026-05-21 17:33:45 +0200

    add DoctorService business layer

--- MedTenant.Application/Services/DoctorService.cs
+++ MedTenant.Application/Services/DoctorService.cs
@@ -1,6 +1,48 @@
-namespace MedTenant.Application.Entities;
+using MedTenant.Application.Entities;
+using MedTenant.Application.Interfaces;
+using System.Collections.Generic;
 
-public class DoctorService
+namespace MedTenant.Application.Services
 {
-    
+    public class DoctorService : IDoctorService
+    {
+        private readonly IDoctorRepository _doctorRepository;
+
+        public DoctorService(IDoctorRepository doctorRepository)
+        {
+            _doctorRepository = doctorRepository;
+        }
+
+        public void AddDoctor(Doctor doctor)
+        {
+            // the name cannot be empty 
+            if (string.IsNullOrEmpty(doctor.FirstName))
+            {
+                throw new Exception("First name cannot be empty");
+            }
+
+            // if accepted move to repository 
+            _doctorRepository.AddDoctor(doctor);
+        }
+
+        public List<Doctor> GetAllDoctors()
+        {
+            return _doctorRepository.GetAllDoctors();
+        }
+
+        public void DeactiveDoctor(int id)
+        {
+            _doctorRepository.DeactiveDoctor(id);
+        }
+
+        public Doctor GetDoctorById(int id)
+        {
+            return _doctorRepository.GetDoctorById(id);
+        }
+
+        public void UpdateDoctor(Doctor doctor)
+        {
+            _doctorRepository.UpdateDoctor(doctor);
+        }
+    }
 }
